913f6a4d90a46b53ddf619c3267a8e7288af468b
Leticia Lima <leticiaalima77@gmail.com>
2025-04-28 08:52:19 -0300

    ,,

--- ex1/Program.cs
+++ ex1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        int valor = 1;
+
+        switch 
+    
+
+
+
+}
+
+    }
